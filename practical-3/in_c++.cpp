38ec09a7bfaaa5393e99c2240fa95d1c1a0ad9bd
yash suthar <98970491+Yash636261@users.noreply.github.com>
2022-11-07 23:10:01 +0530

    Update in_c++.cpp

--- practical-3/in_c++.cpp
+++ practical-3/in_c++.cpp
@@ -1,42 +1,107 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-struct mystack
+int top=-1, stack[100],n,choice,x,pos,val;
+void push()
 {
-    int *arr;
-    int cap;
-    int top;
-    mystack(int c){
-        arr=new int[c];
-        top=-1;
+    if(top>=n-1)
+    {
+        cout<<"\n\tSTACK is over flow";
     }
-    void push(int x){
+    else
+    {
+        cout<<"enter the element you want to insert:"<<endl;
+        cin>>x;
         top++;
-        arr[top]=x;
+        stack[top]=x;
     }
-    int pop(){
-        int res=arr[top];
-        top--;
-        return res;
+}
+void pop(){
+  if( top<0 )
+    cout<<"your stack is underflaw.";
+  else
+    top--;
+}
+void peep(){
+    cout<<"the top most element is "<<stack[top]<<endl;
+}
+void change()
+{
+    cout<<"enter the position for which you want to change:"<<endl;
+    cin>>pos;
+    cout<<"enter the new value:"<<endl;
+    cin>>val;
+    if(top=-1){
+        cout<<"stack is underflow.";
     }
-    int peek(){
-        return arr[top];
+    else{
+        stack[pos]=val;
     }
-    int size(){
-        return (top+1);
+}
+void display()
+{
+    if(top>=0)
+    {
+        cout<<"\n The elements in STACK \n";
+        for(int i=top; i>=0; i--)
+            cout<<"\n"<<stack[i];
     }
-    bool isEmpty(){
-        return (top==-1);
+    else
+    {
+        cout<<"\n The STACK is empty";
     }
-};
+}
 int main()
 {
-    mystack s(5);
-    s.push(5);
-    s.push(10);
-    s.push(20);
-    cout<<s.pop()<<endl;
-    cout<<s.size()<<endl;
-    cout<<s.peek()<<endl;
-    cout<<s.isEmpty()<<endl;
+    top=-1;
+    cout<<"\n Enter the size of STACK[MAX=100]:";
+    cin>>n;
+    cout<<"\n\t STACK OPERATIONS USING ARRAY";
+    cout<<"\n\t--------------------------------";
+    cout<<"\n\t 1.PUSH\n\t 2.POP\n\t 3.PEEP\n\t 4.CHANGE\n\t 5.DISPLAY\n\t 6.EXIT";
+    do
+    {
+        cout<<"\n Enter the Choice:";
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+            {
+                push();
+                break;
+            }
+            case 2:
+            {
+                pop();
+                break;
+            }
+            case 3:
+            {
+                peep();
+                break;
+            }
+            case 4:
+            {
+                change();
+                break;
+            }
+            case 5:
+            {
+                display();
+                break;
+            }
+            case 6:
+            {
+                cout<<"\n\t EXIT POINT ";
+                break;
+            }
+            default:
+            {
+                cout<<"\n\t Please Enter a Valid Choice(1/2/3/4/5)";
+                break;
+            }
+                
+        }
+    }
+    while(choice!=4);
     return 0;
 }
